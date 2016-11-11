f0cc5186906b400e8f80f358b349320958fcc531
Bruno Pastre <pastre68@gmail.com>
2016-11-11 19:48:17 -0200

    Update segundo_trampo.c

--- Arquivo/segundo_trampo.c
+++ Arquivo/segundo_trampo.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#define PATH "C:/Temp/!OpaOba/saves.txt"
+
+#define PATH "!saves.txt"
+
 typedef struct asd{
 	int id;
 	char nome[50];
@@ -10,10 +12,9 @@ typedef struct asd{
 	char data_de_nascimento[10];
 	//Tem um bug que quando ele é inteiro crasha o programa
 	char idade[3];
-	//TODO: Implementar uma lista encadeada
-	struct asd * proximo_registro;
 }REGISTRO;
 
+int imprime_registro(char * , int);
 void escreve_no_arquivo(char *);
 int id_atual = 0;
 
@@ -26,7 +27,7 @@ int main(){
 		if(op == '1'){
 			escreve_no_arquivo(PATH);
 		}else if(op == '2'){
-			
+			imprime_registro(PATH, -1);
 		}
 		else{
 			
@@ -38,11 +39,40 @@ int main(){
 	return 0;
 }
 
+int imprime_registro(char * caminho, int id){
+	FILE * arquivo = NULL;
+	REGISTRO reg;
+	int i;
+	if( (arquivo = fopen(caminho, "rb+") ) == NULL){
+		puts("Deu BO");
+		return 0;
+	}else
+		printf("NAO E NULO! %d\n", sizeof(REGISTRO));
+	
+	if(id < 0){
+		//printf("To no if");
+		
+		for(i = 1; i <= id_atual + 1; i++){
+			fread(&reg, sizeof(REGISTRO), i, arquivo);
+			printf("Nome: %s\n", reg.nome);
+			printf("Endereco %s\n", reg.endereco);
+			printf("Telefone %s\n", reg.telefone);
+			printf("Data de nascimento %s\n", reg.data_de_nascimento );
+			printf("Idade %s\n", reg.idade);	
+		
+		}
+	
+	}else{	
+	}
+	
+	
+}
+
 void escreve_no_arquivo(char * caminho){
-	FILE * arquivo;
+	FILE * arquivo = NULL;
 	char op;
 	if((arquivo = fopen(caminho, "wb+")) == NULL){
-		printf(" BO deu ");	
+		printf(" BO deu \n");	
 		return;
 	}
 	do{	
